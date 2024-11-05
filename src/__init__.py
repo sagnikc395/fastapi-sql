b11fd80b8c552584caad3aeb19298eb905b3f3c0
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-05 11:17:59 +0530

    src: added lifespan event

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,29 @@
 from fastapi import FastAPI 
 
-app = FastAPI()
+#create an lifecycle event to connect to db by init an asyncocntextmanager 
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #define which event when the server starts 
+    # and what to write when the server stops 
+    print(f"Server is starting ...")
+    yield app 
+    print(f"Server is shutting down ...")
+
+
+app = FastAPI(
+    title="mind-map",
+    version="0.1.0",
+    description="A simple example of a FastAPI application",
+    # docs_url="/docs",
+    # redoc_url="/",
+    # openapi_url="/openapi.json"
+    lifespan=lifespan
+)
+
+
 
 ## decorator to make a get request to ping endpoint 
 @app.get("/ping")
