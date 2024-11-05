04910dab5990e745a49bbbbbcb3a266a1a1e2475
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-11-05 11:10:15 +0530

    src: added sample get handler

--- runserver.py
+++ runserver.py
@@ -3,6 +3,6 @@
 import uvicorn 
 
 if __name__ == '__main__':
-    uvicorn.run("src:app", host="127.0.0.1", port=8080)
+    uvicorn.run("src:app", host="127.0.0.1", port=8080,reload=True)
 
     
--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+## decorator to make a get request to ping endpoint 
+@app.get("/ping")
+async def ping():
+    return {
+        "message":"pong"
+    }
